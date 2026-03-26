d7d78a12e69a6d6aae38fa4efd4ce151b92d1fe9
bajajravi12 <ravibajaj12222@gmail.com>
2026-03-26 10:14:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
-unzip v2ray-linux-64.zip
+
+apt update && apt install -y curl unzip
+
+curl -L -o v2ray.zip https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+
+unzip v2ray.zip
+
 chmod +x v2ray
+
 ./v2ray -config=config.json
